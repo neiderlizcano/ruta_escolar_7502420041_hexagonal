8a728aa6e51c4da7fd40e3399b3769a5965ff773
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2025-11-21 22:13:51 -0500

    Inicializa estructura del proyecto referente al flujo correspondiente a la Und 3, cada uno con sus paquetes y la estructura creada

---
+++ src/main/java/co/edu/rutaescolar/ruta_escolar_v2/Ruta_escolar_v2.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.rutaescolar.ruta_escolar_v2;
+
+public class Ruta_escolar_v2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
